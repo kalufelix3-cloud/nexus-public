c3336761bae31cd9dee532ac5f8d531e0abc1247
Matthew Piggott <mpiggott@sonatype.com>
2025-03-19 10:47:51 -0400

    Public export of Nexus code:   - df71ff33efa6221aa54ab07e74e53c5c6baba454 by Matthew Piggott <mpiggott@sonatype.com>
    GitOrigin-RevId: df71ff33efa6221aa54ab07e74e53c5c6baba454

--- components/nexus-base/src/main/java/org/sonatype/nexus/internal/rest/FreezeResourceDoc.java
+++ components/nexus-base/src/main/java/org/sonatype/nexus/internal/rest/FreezeResourceDoc.java
@@ -30,7 +30,7 @@ public interface FreezeResourceDoc
   @ApiOperation("Get read-only state")
   ReadOnlyState get();
 
-  @ApiOperation(value = "Prevent changes to embedded OrientDB",
+  @ApiOperation(value = "Prevent changes to the database",
       notes = "For low-level system maintenance purposes only; do not use if you want users to still be able to download components.")
   @ApiResponses(value = {
       @ApiResponse(code = 204, message = "Database is now read-only"),
@@ -39,7 +39,7 @@ public interface FreezeResourceDoc
   })
   void freeze();
 
-  @ApiOperation(value = "Release read-only and allow changes to embedded OrientDB",
+  @ApiOperation(value = "Release read-only and allow changes to the database",
       notes = "Releases administrator-initiated read-only status. Will not release read-only status caused by system tasks.")
   @ApiResponses(value = {
       @ApiResponse(code = 204, message = "Database is no longer read-only"),
@@ -48,7 +48,7 @@ public interface FreezeResourceDoc
   })
   void release();
 
-  @ApiOperation(value = "Forcibly release read-only and allow changes to embedded OrientDB",
+  @ApiOperation(value = "Forcibly release read-only and allow changes to the database",
       notes = "Forcibly release read-only status, including if caused by system tasks. Warning: may result in data loss.")
   @ApiResponses(value = {
       @ApiResponse(code = 204, message = "Database is no longer read-only"),
