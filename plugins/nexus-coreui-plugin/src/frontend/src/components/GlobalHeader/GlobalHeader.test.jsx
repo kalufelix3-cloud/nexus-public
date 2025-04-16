d5f31ae2d162b32e25419466f5a80ad5b89b6b76
Guillermo Hedzko <ghedzko@gmail.com>
2025-04-16 16:14:59 -0300

    Public export of Nexus code:   - 48b5cd3c3b70fb378ea024c1517d07a23eb83c61 by Guillermo Hedzko <ghedzko@gmail.com>
    GitOrigin-RevId: 48b5cd3c3b70fb378ea024c1517d07a23eb83c61

--- plugins/nexus-coreui-plugin/src/frontend/src/components/GlobalHeader/GlobalHeader.test.jsx
+++ plugins/nexus-coreui-plugin/src/frontend/src/components/GlobalHeader/GlobalHeader.test.jsx
@@ -40,10 +40,10 @@ jest.mock('swagger-ui-react', () => {
 
 // the variable id allows us to simulate being on a react vs on an extJs page
 // for refresh tests
-let welcomeWrapperClassName;
+let welcomeWrapperId;
 jest.mock('../pages/user/Welcome/Welcome', () => {
   return () => {
-    return (<main className={welcomeWrapperClassName}><h1>Welcome Mock</h1></main>);
+    return (<main id={welcomeWrapperId}><h1>Welcome Mock</h1></main>);
   }
 });
 
@@ -82,7 +82,7 @@ describe("GlobalHeader", () => {
   const givenUserName = 'test-user';
 
   beforeEach(() => {
-    welcomeWrapperClassName = "" ;
+    welcomeWrapperId = "some-id-for-a-react-page-" + Math.random();
 
     givenExtJSState()
     jest.spyOn(UpgradeAlertFunctions, 'hasUser').mockReturnValue(hasUserKey);
@@ -535,7 +535,6 @@ describe("GlobalHeader", () => {
     it('performs an appropriate refresh operation for a react rendered component', async () => {
        const { router} = renderComponent();
        jest.spyOn(router.stateService, 'reload');
-       
 
        const banner = screen.getByRole('banner');
        expect(banner).toBeVisible();
@@ -546,9 +545,9 @@ describe("GlobalHeader", () => {
     });
 
     it('performs an appropriate refresh operation for an extjs rendered component', async () => {
-      // render a component with the class name nxrm-ext-js-wrapper so that the logic determines this to be
+      // render a component with the id feature-content so that the logic determines this to be
       // an ExtJS rendered page
-      welcomeWrapperClassName = 'nxrm-ext-js-wrapper';
+      welcomeWrapperId = 'feature-content';
 
       const refresh = jest.spyOn(ExtJS, 'refresh').mockReturnValue(null);
 
