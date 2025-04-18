6a9f96e802d89eb32b9ea62a05d0b38a09671ba1
Michael Martz <mmartz@sonatype.com>
2025-04-18 16:10:55 -0500

    Public export of Nexus code:   - 1b2ca1f64403d8a5da21fe350af18f9774ebca1f by Michael Martz <mmartz@sonatype.com>
    GitOrigin-RevId: 1b2ca1f64403d8a5da21fe350af18f9774ebca1f

--- components/nexus-ui-plugin/src/frontend/src/router/useIsVisible.jsx
+++ components/nexus-ui-plugin/src/frontend/src/router/useIsVisible.jsx
@@ -1,4 +1,4 @@
-/*
+/**
  * Sonatype Nexus (TM) Open Source Version
  * Copyright (c) 2008-present Sonatype, Inc.
  * All rights reserved. Includes the third-party code listed at http://links.sonatype.com/products/nexus/oss/attributions.
@@ -6,20 +6,34 @@
  * This program and the accompanying materials are made available under the terms of the Eclipse Public License Version 1.0,
  * which accompanies this distribution and is available at http://www.eclipse.org/legal/epl-v10.html.
  *
- * Sonatype Nexus (TM) Open Source Version is distributed with Sencha Ext JS pursuant to a FLOSS Exception agreed upon
- * between Sonatype, Inc. and Sencha Inc. Sencha Ext JS is licensed under GPL v3 and cannot be redistributed as part of a
- * closed source work.
- *
  * Sonatype Nexus (TM) Professional Version is available from Sonatype, Inc. "Sonatype" and "Sonatype Nexus" are trademarks
  * of Sonatype, Inc. Apache Maven is a trademark of the Apache Software Foundation. M2eclipse is a trademark of the
  * Eclipse Foundation. All other trademarks are the property of their respective owners.
  */
+<<<<<<<< HEAD:private/plugins/nexus-cloudui-plugin/src/frontend/src/components/UpgradeAlert/UpgradeAlert.scss
+.nx-upgrade-alert {
+  margin: 0;
+  width: 100%;
+  max-width: 100%;
+}
 
-export function waitForExtJs(callback) {
-  const interval = setInterval(() => {
-    if (Ext.getApplication() && NX.Permissions.permissions !== undefined) {
-      clearInterval(interval);
-      callback();
-    }
-  }, 1);
+.upgrade-in-progress-alert {
+  text-align: center;
+}
+
+.upgrade-alert-btn-bar {
+  width: 100%;
+  justify-content: space-between;
+}
+
+.alert-text {
+  display: inline-flex;
+========
+
+import ExtJS from '../interface/ExtJS';
+import isVisible from './isVisible';
+
+export default function useIsVisible(visibilityRequirements) {
+  return ExtJS.useVisiblityWithChanges(() => isVisible(visibilityRequirements));
+>>>>>>>> d414d7b5f3 (NEXUS-46919 reduce shared code burden (#12302)):components/nexus-ui-plugin/src/frontend/src/router/useIsVisible.jsx
 }
