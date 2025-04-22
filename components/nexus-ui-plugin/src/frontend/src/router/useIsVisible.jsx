e11257490271138cbce2f7577382fb39106abb61
kellyrob99 <kellyrob@gmail.com>
2025-04-22 11:25:43 -0700

    Public export of Nexus code:   - 3e0dbe85cf90db04423b2adbf93867cd1050692d by kellyrob99 <kellyrob@gmail.com>
    GitOrigin-RevId: 3e0dbe85cf90db04423b2adbf93867cd1050692d

--- components/nexus-ui-plugin/src/frontend/src/router/useIsVisible.jsx
+++ components/nexus-ui-plugin/src/frontend/src/router/useIsVisible.jsx
@@ -1,4 +1,4 @@
-/**
+/*
  * Sonatype Nexus (TM) Open Source Version
  * Copyright (c) 2008-present Sonatype, Inc.
  * All rights reserved. Includes the third-party code listed at http://links.sonatype.com/products/nexus/oss/attributions.
@@ -10,30 +10,10 @@
  * of Sonatype, Inc. Apache Maven is a trademark of the Apache Software Foundation. M2eclipse is a trademark of the
  * Eclipse Foundation. All other trademarks are the property of their respective owners.
  */
-<<<<<<<< HEAD:private/plugins/nexus-cloudui-plugin/src/frontend/src/components/UpgradeAlert/UpgradeAlert.scss
-.nx-upgrade-alert {
-  margin: 0;
-  width: 100%;
-  max-width: 100%;
-}
-
-.upgrade-in-progress-alert {
-  text-align: center;
-}
-
-.upgrade-alert-btn-bar {
-  width: 100%;
-  justify-content: space-between;
-}
-
-.alert-text {
-  display: inline-flex;
-========
 
 import ExtJS from '../interface/ExtJS';
 import isVisible from './isVisible';
 
 export default function useIsVisible(visibilityRequirements) {
   return ExtJS.useVisiblityWithChanges(() => isVisible(visibilityRequirements));
->>>>>>>> d414d7b5f3 (NEXUS-46919 reduce shared code burden (#12302)):components/nexus-ui-plugin/src/frontend/src/router/useIsVisible.jsx
 }
